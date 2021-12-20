b48ccbff52a54dac317f2dd658de3c60107294cd
Antoine <antoine@avelka.com>
2021-12-20 08:47:39 +0100

    theme switcher style

--- src/assets/js/theme-switch.js
+++ src/assets/js/theme-switch.js
@@ -1,6 +1,10 @@
 const switcher = document.querySelector('#theme-switcher')
 const doc = document.firstElementChild;
 const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+const toggleDarkTheme = dark => {
+  switcher.querySelector('#dark').checked = dark;
+  doc.setAttribute('color-scheme',  dark ? "dark" : "light");
+}
 
 darkModeMediaQuery.onChange = ((e) => {
   const darkModeOn = e.matches;
@@ -8,10 +12,7 @@ darkModeMediaQuery.onChange = ((e) => {
 });
 
 console.log(doc, darkModeMediaQuery);
+toggleDarkTheme(darkModeMediaQuery.matches);
 switcher.addEventListener('input', e =>
 toggleDarkTheme(e.target.checked))
 
-const toggleDarkTheme = dark => {
-
-  doc.setAttribute('color-scheme',  dark ? "dark" : "light");
-}
